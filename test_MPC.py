0220200747f3f274c8655ec28e1f7599878090c0
Francisco Escobar <francisco.escobarprado@ucr.ac.cr>
2023-07-23 11:23:07 -0600

    Test orchestration of all modules (PATH managing is still poor)

--- test_MPC.py
+++ test_MPC.py
@@ -10,24 +10,168 @@ sys.path.append("src")
 
 # Modules from this repository
 import test_systems
+import experiment
+import sim_interaction
+import metrics
+import nli
+import visual
+import records
+
 
 # Modules from the standard library
 
 
 # Other modules
 
-if __name__ == "__main__":
+# The following functions define experiments ready to be run.
 
+def simulate_DERAs(penetration: float = 0.1) -> experiment.Experiment:
+    """
+    Simulate the DERAs with and without the line tripping.
+    """
 
-    nordic = test_systems.get_dynamic_Nordic()
+    # Import the test system with a certain DERA penetration.
+    nordic = test_systems.get_Nordic_with_DERAs(penetration=penetration)
 
-    print(
-        nordic.generate_table(
-            show_lines=False,
-            show_transformers=False,
+    # Add the detectors to the system (NLIs and field currents).
+    nordic.add_detector(
+        detector=nli.NLI(
+            observed_corridor=("4041", ["4031"]),
+            h=20e-3,
+            delta_T=7,
+            tau_s=1,
+            epsilon=1e-3,
         )
     )
 
-    nordic = test_systems.get_Nordic_with_DERAs(penetration=0.5)
+    nordic.add_detector(
+        detector=nli.NLI(
+            observed_corridor=("4042", ["4021", "4032"]),
+            h=20e-3,
+            delta_T=7,
+            tau_s=1,
+            epsilon=1e-3,
+        )
+    )
 
-    print(nordic.generate_table(show_lines=False, show_transformers=False))
+    for generator in nordic.generators:
+        nordic.add_detector(
+            detector=nli.FieldCurrent(machine_name=generator.name)
+        )
+
+
+    # Initialize the experiment.
+    exp = experiment.Experiment(
+        name="Big DERAs",
+        DLL_dir=r"C:\Users\FranciscoEscobarPrad\Desktop\URAMSES-1.2\Release_intel_w64"
+    )
+
+    # Add to the experiment the desired disturbances (the case without
+    # disturbance will be run by default).
+    exp.add_disturbances(
+        "4032-4044",
+        sim_interaction.Disturbance(
+            ocurrence_time=1.0,
+            object_acted_on=nordic.get_bus(name="4032"),
+            par_name="fault",
+            par_value=1e-4
+        ),
+        sim_interaction.Disturbance(
+            ocurrence_time=1.1,
+            object_acted_on=nordic.get_bus(name="4032"),
+            par_name="clearance",
+            par_value=None
+        ),
+        sim_interaction.Disturbance(
+            ocurrence_time=1.1,
+            object_acted_on=nordic.get_branches_between(
+                bus_name_1="4032",
+                bus_name_2="4044"
+            )[0],
+            par_name="status",
+            par_value=0
+        )
+    )
+
+    # # Add to the experiment the desired visualizations.
+    # exp.add_visualizations(
+    #     visual.NLI_plots(
+    #         receiving_buses=["4041", "4042"]
+    #     ),
+    #     visual.CentralVoltages()
+    # )
+
+    # Add to the experiment all transmission quantities as observables
+    # (implementing a nicer interface would be expensive).
+    for bus in nordic.buses:
+        exp.add_observables(
+            sim_interaction.Observable(
+                observed_object=bus,
+                obs_name="BV"
+            )
+        )
+
+    for generator in nordic.generators:
+        exp.add_observables(
+            sim_interaction.Observable(
+                observed_object=generator,
+                obs_name="if"
+            )
+        )
+
+    for record in nordic.records:
+        if isinstance(record, records.DCTL):
+            exp.add_observables(
+                sim_interaction.Observable(
+                    observed_object=record,
+                    obs_name=None
+                )
+            )
+
+    for injector in nordic.injectors:
+        if isinstance(injector, records.DERA):
+            exp.add_observables(
+                sim_interaction.Observable(
+                    observed_object=injector,
+                    obs_name="Pgen"
+                ),
+                sim_interaction.Observable(
+                    observed_object=injector,
+                    obs_name="Qgen"
+                )
+            )
+
+    # Add to the experiment the system.
+    exp.add_system(description="Big DERAs", system=nordic)
+
+    # Add to the experiment the metrics.
+    exp.add_metrics(
+        metrics.VoltageIntegral(only_central=True),
+        metrics.ReactiveMargin(only_central=True),
+        metrics.ControlEffort(power_type="P"),
+        metrics.ControlEffort(power_type="Q"),
+        metrics.TapMovements(only_central=True),
+        metrics.NLI(),
+        metrics.PowerReserve(),
+        metrics.ActivatedOELs(only_central=False),
+    )
+
+    # Set the solver and the horizon
+    exp.set_solver_and_horizon(
+        solver_settings_dict={},
+        horizon=200,
+    )
+
+    return exp, nordic
+
+if __name__ == "__main__":
+
+    #############################################
+    # Experiment: Simulation of DERAs without MPC
+    #############################################
+
+    p = 0.0
+    exp, nordic = simulate_DERAs(penetration=p)
+    print(f"Running experiment with DERA penetration = {p*100:.0f} %...")
+    exp.run()
+
